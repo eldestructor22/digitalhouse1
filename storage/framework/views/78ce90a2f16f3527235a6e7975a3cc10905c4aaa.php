15f912fa4b1054f0f65c074dbcbc5b1eb1b15733
eldestructor22 <49496461+eldestructor22@users.noreply.github.com>
2019-07-30 13:40:22 -0300

    Add files via upload

---
+++ storage/framework/views/78ce90a2f16f3527235a6e7975a3cc10905c4aaa.php
@@ -0,0 +1,25 @@
+<a href="crearPregunta.php">Agregar nueva Pregunta</a>
+
+<br>
+<?php
+
+
+ $consulta = $conex->query("SELECT * FROM preguntas ORDER BY id DESC");
+ $consulta->execute();
+ $preguntas = $consulta->fetchAll(PDO::FETCH_ASSOC);
+
+echo "PREGUNTAS";
+ foreach ($preguntas as $pregunta) {
+   echo "<br>$pregunta[enunciado]<br>";
+}
+$consultita = $conex ->query("SELECT * FROM respuestas ORDER BY id DESC");
+$consultita-> execute();
+$respuestas = $consultita->fetchALL(PDO::FETCH_ASSOC);
+
+echo "<br> RESPUESTAS";
+
+foreach ($respuestas as $respuesta) {
+ echo "<br>$respuesta[enunciado]<br>";
+}
+?>
+<?php /**PATH /media/sf_Compartido/ultimate_quizz/resources/views/preguntas.blade.php ENDPATH**/ ?>
