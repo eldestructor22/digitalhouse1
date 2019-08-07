b627bddc79ab50e80c45c18ea1dd6e50bc26f4fa
eldestructor22 <49496461+eldestructor22@users.noreply.github.com>
2019-08-06 21:39:32 -0300

    Add files via upload

---
+++ storage/framework/views/127b098952f81c8979a71bfecd3c488e24b9162e.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js">
+    </script>
+  </head>
+  <body>
+    <div class="container">
+      <form class="" action="/juego/test" method="post">
+        <?php echo csrf_field(); ?>
+
+      <div class="pregunta1">
+        <?php $__currentLoopData = $preguntas; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $pregunta): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+          <li><?php echo e($pregunta->enunciado); ?></li>
+          <?php $__currentLoopData = $pregunta->respuestas; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $respuestita): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+            <input type="radio" name="respuesta[<?php echo e($pregunta->id); ?>]" value="<?php echo e($respuestita->id); ?>"><?php echo e($respuestita->enunciado); ?>
+
+          <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+      </div>
+      <button type="submit" name="button">Responder</button>
+            </form>
+      
+    </div>
+  </body>
+</html>
+<?php /**PATH C:\Users\Maru\Desktop\ultimate_quizz\digitalhouse1-master\resources\views/juego.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/b15d332cf4d8ccc452232fd2d49d8ebc9671b5ca.php
@@ -0,0 +1,7 @@
+<?php echo $__env->make('HomeUsuario', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+<?php $__env->startSection('content'); ?>
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Maru\Desktop\ultimate_quizz\digitalhouse1-master\resources\views/home.blade.php ENDPATH**/ ?>
